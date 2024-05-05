895211bc82f35de5439d28805c14036f21eec274
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-05 04:22:06 +0300

    reports

--- src/App/Models/RepotModel.php
+++ src/App/Models/RepotModel.php
@@ -59,31 +59,31 @@ class ReportModel extends DBStorage implements ModelInterface
             'message' => $message,
         ]);
 
-        // $this->skipOrRemving($type, $reportedId);
+        $this->skipOrRemving($type, $reportedId);
     }
 
-    // public function skipOrRemving(string $type, int $reportedId)
-    // {
+    public function skipOrRemving(string $type, int $reportedId)
+    {
 
-    //     $countQuery = "SELECT COUNT(*) as count FROM {$this->__tablename__} WHERE type = :type AND reportedId = :reportedId";
-    //     $countResult = $this->db->query($countQuery, [
-    //         'type' => $type,
-    //         'reportedId' => $reportedId,
-    //     ])->find();
+        $countQuery = "SELECT COUNT(*) as count FROM {$this->__tablename__} WHERE type = :type AND reportedId = :reportedId";
+        $countResult = $this->db->query($countQuery, [
+            'type' => $type,
+            'reportedId' => $reportedId,
+        ])->find();
 
         
-    //     if ($countResult['count'] && $type == "users")
-    //     {
-    //         $query = "SELECT COUNT(*)
-    //                   FROM users u
-    //                   INNER JOIN friends f ON u.id IN (f.receiverId, f.senderId)
-    //                   WHERE u.id != :reportedId AND :reportedId IN (f.receiverId, f.senderId) AND status = 2";
+        if ($countResult['count'] && $type == "users")
+        {
+            $query = "SELECT COUNT(*)
+                      FROM users u
+                      INNER JOIN friends f ON u.id IN (f.receiverId, f.senderId)
+                      WHERE u.id != :reportedId AND :reportedId IN (f.receiverId, f.senderId) AND status = 2";
 
-    //         $count = $this->db->query($query, ['userId' => $reportedId])->findAll();
-    //         if (($countResult / (int)$count) * 100 >= 60) {
-    //             $query = "DELETE FROM type WHERE id = :reportedId";
-    //             $this->db->query($query, ['id' => $reportedId]);
-    //         }
-    //     }
-    // }
+            $count = $this->db->query($query, ['userId' => $reportedId])->findAll();
+            if (($countResult / (int)$count) * 100 >= 60) {
+                $query = "DELETE FROM type WHERE id = :reportedId";
+                $this->db->query($query, ['id' => $reportedId]);
+            }
+        }
+    }
 }
